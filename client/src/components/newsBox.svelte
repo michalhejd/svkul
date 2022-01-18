270398887bd67d7342d29b5d73a68cc546e1c538
michalhejd <71251670+michalhejd@users.noreply.github.com>
2022-01-18 16:39:23 +0100

    responsive boxy

--- client/src/components/newsBox.svelte
+++ client/src/components/newsBox.svelte
@@ -1,16 +1,28 @@
 <div class="newsBox">
-    <img src="https://i.ibb.co/4jpdkRk/image1.png" alt="">
-<p>something</p>
+    <img src="https://i.ibb.co/4jpdkRk/image1.png" alt="" />
+    <div class="text">
+        <h1>Název</h1>
+        <h3>Cena</h3>
+    </div>
 </div>
+
 <style>
-    .newsBox{
+    .newsBox {
         padding: 20px;
         display: flex;
+
         flex-direction: column;
         background-color: white;
     }
-    img{
+    img {
         width: 100%;
         height: 100%;
+    }
+    .text {
+        display: flex;
+        flex-direction: column;
+    }
+    h1,h3{
+        margin: 20px 0 0 0;
     }
 </style>
