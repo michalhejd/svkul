ecfe2046d0cbb38fb6b62f185f2c61afdf7efd92
Daniel Bulant <danbulant@gmail.com>
2022-02-10 09:42:51 +0100

    remove double nav

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,12 +1,10 @@
 <script>
-	import Nav from "../components/nav.svelte"
-	import SlideBox from "../components/slideBox.svelte"
-	import News from "../components/news.svelte"
+	import SlideBox from "../components/slideBox.svelte";
+	import News from "../components/news.svelte";
 	import AboutProject from "../components/aboutProject.svelte";
 </script>
 
 <main>
-	<Nav></Nav>
 	<SlideBox></SlideBox>
 	<News></News>
 	<AboutProject></AboutProject>
