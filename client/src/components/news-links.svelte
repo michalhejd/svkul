80a88fa3bbd49fcd8331eacb206ab587d6030e7f
michalhejd <71251670+michalhejd@users.noreply.github.com>
2022-02-11 18:07:22 +0100

    a

---
+++ client/src/components/news-links.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+<div class="link">
+
+    <p>alsnfos</p>
+</div>
+<style>
+    .link{
+        box-sizing: border-box;
+        width: 100%;
+        padding: 15px;
+        background-color: white;
+        border-radius: 5px;
+    }
+    p{
+        font-family: 'Montserrat', sans-serif;
+    }
+</style>
