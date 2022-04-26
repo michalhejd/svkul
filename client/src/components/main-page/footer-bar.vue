726304936ed4bc5a7c93591989c81ec308d22e8c
michalhejd <71251670+michalhejd@users.noreply.github.com>
2022-04-26 09:58:54 +0200

    nevim

--- client/src/components/main-page/footer-bar.vue
+++ client/src/components/main-page/footer-bar.vue
@@ -1,14 +1,40 @@
 <style lang="scss" scoped>
+.footer-bar{
+ width: 100%;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  padding-left: 80px;
+  padding-right: 80px;
+  background-color: #f5f5f5;
+  }
 </style>
 <template>
   <div class="footer-bar">
-    <div class="ack"></div>
+    <div class="ack">
+      <carousel
+        :dots="false"
+        :nav="false"
+        :items="1"
+        :loop="true"
+        :autoPlay="true"
+        :autoWidth="true"
+        :pullDrag="false"
+        :touchDrag="false"
+        :mouseDrag="false"
+      >
+      </carousel>
+    </div>
     <div class="declaration"></div>
     <div class="copyright"></div>
   </div>
 </template>
 <script>
+import carousel from "vue-owl-carousel";
+
 export default {
   name: "footer-bar",
+  components: {
+    carousel,
+  },
 };
 </script>
