20ac7bde33741a1b948be4f3934e194f3aca1419
michalhejd <71251670+michalhejd@users.noreply.github.com>
2022-10-29 15:52:23 +0200

    empty commit

--- client/plugins/fontawesome.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAngleDown, faBars, faXmark, faPenToSquare, faPlus, faUser, faUpload, faMagnifyingGlass, faBug  } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add( faAngleDown, faBars, faXmark, faPenToSquare, faPlus, faUser, faUpload, faMagnifyingGlass, faBug )
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-
