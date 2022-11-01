62943b73b4d61bfdf0d04cc9c05c673b1df9f092
michalhejd <71251670+michalhejd@users.noreply.github.com>
2022-11-01 02:16:36 +0100

    with credentials to axios plugin

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -1,6 +1,7 @@
 export default ({ $axios, redirect }) => {
     //only when development
     if (process.env.NODE_ENV === 'development') {
+        $axios.defaults.withCredentials = true
         $axios.onRequest(config => {
             console.log('Making request to ' + config.url)
         })
