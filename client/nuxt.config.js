708e62820dbd8688651b193501552eda06cf9141
michalhejd <71251670+michalhejd@users.noreply.github.com>
2022-11-04 23:19:27 +0100

    api

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -52,7 +52,7 @@ export default {
     '@nuxtjs/axios',
   ],
   axios: {
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '/api',
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://svkul-api.hejduk.tech',
     credentials: true
   },
   //allow vuetify maps production
